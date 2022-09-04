49eaf7b904b556d36865482d6de0334a37fbf9fd
VasiliCheb <skimanvasia@gmail.com>
2022-09-05 00:08:48 +0300

    Подконка ПП на wide-vp

--- source/js/main.js
+++ source/js/main.js
@@ -9,33 +9,3 @@ window.addEventListener('scroll', () => {
     header.classList.remove('page-header--fixed');
   }
 });
-
-/*const body = document.querySelector('.page__body');
-const headerMenu = body.querySelector('.page-header__menu');
-const navToggle = headerMenu.querySelector('.page-header__menu-toggle');
-const navLinkList = headerMenu.querySelectorAll('.page-header__menu-list a');
-
-headerMenu.classList.remove('page-header__menu--nojs');
-
-if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    body.classList.toggle('page__body--noscroll');
-    headerMenu.classList.toggle('page-header__menu--opened');
-  });
-
-  body.addEventListener('click', (evt) => {
-    if (!headerMenu.contains(evt.target)) {
-      headerMenu.classList.remove('page-header__menu--opened');
-      body.classList.remove('page__body--noscroll');
-      }
-  });
-
-  navLinkList.forEach(item => {
-    item.addEventListener('click', (evt) => {
-      if (headerMenu.contains(evt.target)) {
-        body.classList.remove('page__body--noscroll');
-        headerMenu.classList.remove('page-header__menu--opened');
-      }
-    });
-  });
-}*/
