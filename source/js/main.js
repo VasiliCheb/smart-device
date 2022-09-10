2425480ff6bdd2a487207d7a02326458b2b2c371
VasiliCheb <skimanvasia@gmail.com>
2022-09-10 21:09:55 +0300

    Оживление JS. Сделал Модальное окно. Правки блока аккордион в Футере

--- source/js/main.js
+++ source/js/main.js
@@ -3,10 +3,18 @@ const header = body.querySelector('.page-header');
 const aboutCompanyBlock = body.querySelector('.about-company');
 const aboutCompanyToggle = aboutCompanyBlock.querySelector('.about-company__button');
 
+const modal = body.querySelector('.modal--close');
+const modalOpenedButton = header.querySelector('.page-header__button');
+const modalClosedButton = modal.querySelector('.modal__button-close');
+const inputName = body.querySelector('#modal-user-name');
+const pageMain = body.querySelector('.page-main');
+const footer = body.querySelector('.page-footer');
+
 
 body.classList.remove('page__body--nojs');
 
-/*фиксация шапки при начале скролла*/
+
+//фиксация Шапки при начале скролла
 window.addEventListener('scroll', () => {
   let scrollDistance = window.scrollY;
 
@@ -18,14 +26,55 @@ window.addEventListener('scroll', () => {
 });
 
 
-/*открытие текста в блоке about-сompany*/
+//Модальное окно
+getOpenedModal = () => {
+  modal.style.display = "block";
+  body.classList.add('page__body--noscroll');
+  inputName.focus();
+  header.setAttribute('inert', '');
+  pageMain.setAttribute('inert', '');
+  footer.setAttribute('inert', '');
+
+  getClosetModal = () => {
+    modal.style.display = "none";
+    body.classList.remove('page__body--noscroll');
+    header.removeAttribute("inert");
+    pageMain.removeAttribute("inert");
+    footer.removeAttribute("inert");
+    modalOpenedButton.focus();
+  }
+}
+
+modalOpenedButton.onclick = () => {
+  getOpenedModal();
+}
+
+modalClosedButton.onclick = () => {
+  getClosetModal();
+}
+
+window.onclick = (evt) => {
+  if (evt.target == modal) {
+    getClosetModal();
+  }
+}
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    getClosetModal();
+  }
+});
+
+
+//открытие текста в блоке about-сompany
 if (aboutCompanyToggle) {
   aboutCompanyToggle.addEventListener('click', () => {
     aboutCompanyBlock.classList.toggle('about-company--opened');
   });
 }
 
-/*аккордион разделов в футере*/
+
+//аккордион разделов в футере
 class ItcAccordion {
   constructor(target, config) {
     this._el = typeof target === 'string' ? document.querySelector(target) : target;
