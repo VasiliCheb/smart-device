06ed318ba6e737c977895376c261e9375ec89ca7
VasiliCheb <skimanvasia@gmail.com>
2022-09-11 20:45:13 +0300

    Оживление JS. Создание маски для input телефона. Мелкие правки по чек листу

--- source/js/main.js
+++ source/js/main.js
@@ -10,6 +10,8 @@ const inputName = body.querySelector('#modal-user-name');
 const pageMain = body.querySelector('.page-main');
 const footer = body.querySelector('.page-footer');
 
+const inputTel = body.querySelectorAll('.form__field--tel input')
+
 
 body.classList.remove('page__body--nojs');
 
@@ -73,6 +75,43 @@ if (aboutCompanyToggle) {
   });
 }
 
+
+//Маска input телефона в формате: +7(
+window.addEventListener("DOMContentLoaded", () => {
+  [].forEach.call(inputTel,(input) => {
+  var keyCode;
+  mask(evt) = () => {
+      evt.keyCode && (keyCode = evt.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) evt.preventDefault();
+      var matrix = "+7(___) ___ ____",
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, ""),
+          new_value = matrix.replace(/[_\d]/g, (a) => {
+              return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+          });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+          i < 4 && (i = 3);
+          new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substring(0, this.value.length).replace(/_+/g, (a) => {
+              return "\\d{1," + a.length + "}"
+          }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 4 || keyCode > 47 && keyCode < 58) this.value = new_value;
+      if (evt.type == "blur" && this.value.length < 4)  this.value = ""
+  }
+
+  input.addEventListener("input", mask, false);
+  input.addEventListener("focus", mask, false);
+  input.addEventListener("blur", mask, false);
+  input.addEventListener("keydown", mask, false)
+
+});
+
+});
 
 //аккордион разделов в футере
 class ItcAccordion {
