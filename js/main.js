9744ca84bf27cb46088693e2357f36538a184c63
VasiliCheb <87862701+VasiliCheb@users.noreply.github.com>
2022-09-13 19:01:48 +0000

    Deploying to gh-pages from @ VasiliCheb/smart-device@06aafb5fdc8a58e0a70406a96549531d54943651 🚀

---
+++ js/main.js
@@ -0,0 +1,1 @@
+import"./text-show.js";import"./modal.js";import"./mask-input-tel.js";import"./accordion.js";const body=document.querySelector(".page__body");body.classList.remove("page__body--nojs");
